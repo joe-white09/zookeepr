bcd540097829d8af266bb7e94829cfdf1ba31454
joe-white09 <joe_white09@yahoo.com>
2022-06-22 15:14:59 -0500

    connected frontend to server

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(express.static('public'));
+
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
@@ -111,6 +113,19 @@ app.post('/api/animals', (req, res) => {
         const animal = createNewAnimal(req.body, animals);   
         res.json(req.body);
     }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public.index.html'));
 });
 
 app.listen(PORT, () => {
